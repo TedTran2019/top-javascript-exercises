70f0f763590f335b220c7a9c018167fab9050cb4
Ted Tran <tedtran2019@gmail.com>
2023-12-08 10:41:39 -0800

    Completed 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, repeat) {
+  let newStr = "";
+  for (i = 0; i < repeat; i++) {
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
