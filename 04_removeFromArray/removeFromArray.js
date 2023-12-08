a781971fb718fe872c8d1bdba69fe6f731cbd3a9
Ted Tran <tedtran2019@gmail.com>
2023-12-08 11:04:07 -0800

    Completed 03 and 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...targets) {
+  newArr = [];
+  for (ele of arr) {
+    if (!targets.includes(ele)) newArr.push(ele);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
