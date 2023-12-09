c2c35fe73f5c3fb21d55d2f98eaaf20256dda169
Ted Tran <tedtran2019@gmail.com>
2023-12-08 18:11:27 -0800

    06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  return roundToOneDecimal((temperature - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function () {
+  return roundToOneDecimal(temperature * (9 / 5) + 32);
 };
+
+function roundToOneDecimal(nbr) {
+  return Math.round(nbr * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
